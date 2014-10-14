bedf9f0871f0b78c3f0ff88012276ee4d1da1c26
Steve Nolen <technolengy@gmail.com>
2014-10-14 11:37:56 -0700

    script

---
+++ bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#bootstraps the mobilize-in-a-box installer. so the command can easily be executed from the web and then clone the repo from there and get to work
+
+if [ "$EUID" -ne 0 ]
+  then echo "Script needs to be run as root" 
+  exit 1 
+fi
+
+#we'll use git alot, so we need that, certainly
+apt-get -y install git < "/dev/null"
+
+#now that that's done, clone the mobilize-in-a-box repo
+git clone https://github.com/stevenolen/mobilize-in-a-box /opt/mobilize-in-a-box
+chmod +x /opt/mobilize-in-a-box/setup.sh
+
+#prepared.
+echo "Setup is now prepared. Please execute /opt/mobilize-in-a-box/setup.sh as root to set up"
+exit 0
