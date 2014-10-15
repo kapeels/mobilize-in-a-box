60b255e18ccd52b9277761d03629ff5e94152753
stevenolen <technolengy@gmail.com>
2014-10-15 08:36:47 -0700

    make script output easier to follow

--- setup.sh
+++ setup.sh
@@ -6,17 +6,24 @@ if [ "$EUID" -ne 0 ]
 fi
 
 #need to add a few ppas for mariadb and nginx
+echo "################## Installing: python-software-properties  ##################" 
 apt-get install -y python-software-properties  < "/dev/null" &>> /opt/mobilize-in-a-box/run.log
+echo "################## Installing: mariadb 10.0 ppa  ##################" 
 apt-key adv --recv-keys --keyserver hkp://keyserver.ubuntu.com:80 0xcbcb082a1bb943db &> /opt/mobilize-in-a-box/run.log
 add-apt-repository 'deb http://sfo1.mirrors.digitalocean.com/mariadb/repo/10.0/ubuntu trusty main' &> /opt/mobilize-in-a-box/run.log
+echo "################## Installing: opencpu ppa  ##################" 
 add-apt-repository -y ppa:opencpu/opencpu-1.4 &> /opt/mobilize-in-a-box/run.log
+echo "################## Installing: nginx ppa  ##################" 
 add-apt-repository -y ppa:nginx/stable &> /opt/mobilize-in-a-box/run.log
+echo "################## Updating apt packages  ##################" 
 apt-get update &> /opt/mobilize-in-a-box/run.log
 
 #let's install the rest of the dependencies we need from apt
 #TODO: right now we need to actually install x11 in order to get javac to compile the server/frontend..
+echo "################## Installing: openjdk-7-jdk  ##################" 
 apt-get -y install openjdk-7-jdk --no-install-recommends < "/dev/null" &> /opt/mobilize-in-a-box/run.log
 export DEBIAN_FRONTEND=noninteractive
+echo "################## Installing: $(cat required_packages)  ##################" 
 apt-get -y install $(cat required_packages) < "/dev/null" &> /opt/mobilize-in-a-box/run.log
 
 #let's install the rest of the dependencies we need from apt
@@ -24,28 +31,33 @@ mkdir -p /opt/mobilize-in-a-box/git && cd /opt/mobilize-in-a-box/git
 while read line
 do
   repo=$line
-  git clone $repo &> /opt/mobilize-in-a-box/run.log
+  echo "################## Git clone: $repo  ##################"
+  git clone $repo &> /opt/mobilize-in-a-box/run.log &> /opt/mobilize-in-a-box/run.log
 done < /opt/mobilize-in-a-box/required_git_repos
 
 #misc other packages we need. dokuwiki for a lightweight wiki
-wget -P /opt/mobilize-in-a-box/ http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz
-tar zxf /opt/mobilize-in-a-box/dokuwiki-stable.tgz -C /opt/mobilize-in-a-box/
+echo "################## Installing: dokuwiki  ##################"
+wget -P /opt/mobilize-in-a-box/ http://download.dokuwiki.org/src/dokuwiki/dokuwiki-stable.tgz &> /opt/mobilize-in-a-box/run.log
+tar zxf /opt/mobilize-in-a-box/dokuwiki-stable.tgz -C /opt/mobilize-in-a-box/ &> /opt/mobilize-in-a-box/run.log
 rm /opt/mobilize-in-a-box/dokuwiki-stable.tgz
 
 #######compile the server#######
+echo "################## Compiling: ohmageServer  ##################"
 cd /opt/mobilize-in-a-box/git/ohmageServer
-git checkout ohmage-2.16-user_setup_password
+git checkout ohmage-2.16-user_setup_password &> /opt/mobilize-in-a-box/run.log
 ant clean dist &> /opt/mobilize-in-a-box/run.log
-service tomcat7 stop
+service tomcat7 stop &> /opt/mobilize-in-a-box/run.log
 cp dist/webapp-ohmage-2.16.1-no_ssl.war /var/lib/tomcat7/webapps/app.war
 
 #we also need some directories for ohmage to store data
+echo "################## Preparing: /var/lib/ohmage/{audio,audits,documents,images,videos}  ##################"
 mkdir -p /var/log/ohmage
 mkdir -p /var/lib/ohmage/{audio,audits,documents,images,videos}
 chown -R tomcat7.tomcat7 /var/log/ohmage
 chown -R tomcat7.tomcat7 /var/lib/ohmage
 
 #######prepare the db.########
+echo "################## Preparing: ohmage db  ##################"
 dbpw=`date | md5sum | head -c20`
 mysql -uroot -e 'create database ohmage; grant all on ohmage.* to "ohmage"@"localhost" identified by "'$dbpw'"; flush privileges;'
 #remove the create database lines in the first file. who put these there?!
@@ -59,26 +71,29 @@ done
 mysql -uohmage --password="$dbpw" ohmage -e 'update preference set p_value = replace(p_value, "/opt/ohmage/userdata", "/var/lib/ohmage") where p_value like "%userdata%"; update preference set p_value = replace(p_value, "/opt/ohmage/logs", "/var/lib/ohmage") where p_value like "%audit%";'
 
 #compile the gwt frontend
+echo "################## Compiling: ohmage gwt-frontend  ##################"
 cd /opt/mobilize-in-a-box/git/gwt-front-end
-git checkout mobilize
-ant clean build buildwar
+git checkout mobilize &> /opt/mobilize-in-a-box/run.log
+ant clean build buildwar &> /opt/mobilize-in-a-box/run.log
 rm -rf /opt/mobilize-in-a-box/git/gwt-front-end/extracted/
 mkdir /opt/mobilize-in-a-box/git/gwt-front-end/extracted/
 cd /opt/mobilize-in-a-box/git/gwt-front-end/extracted/
-jar xvf ../MobilizeWeb.war
+jar xvf ../MobilizeWeb.war &> /opt/mobilize-in-a-box/run.log
 mkdir -p /var/www/webapps/web
 cp -r * /var/www/webapps/web
 
 #compile the generalized dashboard
+echo "################## Compiling: generlized dashboard  ##################"
 cd /opt/mobilize-in-a-box/git/dashboard
 #installing nodejs from debian is kinda crap
 ln -s /usr/bin/nodejs /usr/bin/node
 export PATH=$PATH:/usr/local/share/npm/bin/
-npm -g install jade recess uglify-js
-make CAMPAIGN=snack OUT=/var/www/webapps/publicdashboard
-make CAMPAIGN=snack OUT=/var/www/webapps/dashboard
+npm -g install jade recess uglify-js &> /opt/mobilize-in-a-box/run.log
+make CAMPAIGN=snack OUT=/var/www/webapps/publicdashboard &> /opt/mobilize-in-a-box/run.log
+make CAMPAIGN=snack OUT=/var/www/webapps/dashboard &> /opt/mobilize-in-a-box/run.log
 
 #move the other www code to it's rightful location
+echo "################## Preparing: copying www projects to /var/www  ##################"
 cp -r /opt/mobilize-in-a-box/git/campaignAuthoringTool /var/www/webapps/; mv /var/www/webapps/campaignAuthoringTool /var/www/webapps/authoring
 cp -r /opt/mobilize-in-a-box/git/campaign_monitor /var/www/webapps/; mv /var/www/webapps/campaign_monitor /var/www/webapps/monitor
 cp -r /opt/mobilize-in-a-box/git/teacher /var/www/webapps/
@@ -88,6 +103,7 @@ cp -ur /opt/mobilize-in-a-box/git/wiki/* /var/www/wiki/data/
 chown -R www-data.www-data /var/www
 
 #copy our config files into place
+echo "################## Preparing: copying ohmage and nginx conf files  ##################"
 cp /opt/mobilize-in-a-box/files/ohmage /etc/ohmage.conf
 cp /opt/mobilize-in-a-box/files/nginx /etc/nginx/sites-available/ohmage
 ln -s /etc/nginx/sites-available/ohmage /etc/nginx/sites-enabled/
@@ -104,9 +120,11 @@ sed -i "s/db.password={DB_PASSWORD_HERE}/db.password=$dbpw/g" /etc/ohmage.conf
 echo 'JAVA_OPTS="$JAVA_OPTS -Djava.net.preferIPv4Stack=true -Djava.net.preferIPv4Addresses"' >> /usr/share/tomcat7/bin/setenv.sh
 
 #we're done!
-echo "Looks like everything is set up. For your records: "
+echo "################## Finished! ##################"
+echo \n\n"Looks like everything is set up. For your records: "
 echo "mysql user ohmage has a password now set to: "$dbpw
 
 ######## start up some stuff ########
-service nginx restart
-service tomcat7 start
+service nginx restart &> /opt/mobilize-in-a-box/run.log
+service tomcat7 start &> /opt/mobilize-in-a-box/run.log
+service apache2 restart &> /opt/mobilize-in-a-box/run.log
