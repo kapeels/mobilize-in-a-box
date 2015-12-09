0e144e3ee9e487a2fb02f720423fa98d89a29499
Steve Nolen <technolengy@gmail.com>
2015-12-09 15:26:15 -0800

    add mobilizingcs/wiki. not working properly under subdir yet.

---
+++ wiki/dokuwiki-local.php
@@ -0,0 +1,17 @@
+<?php
+
+$conf['title'] = 'Mobilize Wiki';
+$conf['start'] = 'home';
+$conf['template'] = 'vector';
+$conf['license'] = 'cc-by-sa';
+$conf['breadcrumbs'] = 0;
+$conf['htmlok'] = 1;
+$conf['target']['extern'] = '_blank';
+$conf['updatecheck'] = 0;
+$conf['userewrite'] = '1';
+$conf['useslash'] = 1;
+$conf['tpl']['vector']['vector_discuss'] = 0;
+$conf['tpl']['vector']['vector_copyright_default'] = 0;
+$conf['tpl']['vector']['vector_loaduserjs'] = 1;
+$conf['datadir'] = './data/git/pages';
+$conf['mediadir'] = './data/git/media';
---
+++ wiki/run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+set -e
+
+
+/usr/sbin/php5-fpm --fpm-config /etc/php5/fpm/php-fpm.conf --pid /var/run/php5-fpm.pid
+/usr/sbin/nginx -g "daemon off;"
